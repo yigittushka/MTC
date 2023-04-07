ff951657dedc42896bce59d9afbb6799cecc69be
Yigit <86945115+yigittushka@users.noreply.github.com>
2023-04-07 14:16:06 +0300

    Update Main.java

--- IdeaProjects/MTC/src/Main.java
+++ IdeaProjects/MTC/src/Main.java
@@ -7,31 +7,35 @@ import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.zip.GZIPInputStream;
+
 public class Main {
     public static void main(String[] args) {
-
+        // получаем текущую дату
         LocalDateTime now = LocalDateTime.now();
+
+        // формируем имя архива
         String filename = String.format("smb_%s_app01.log.gz", now.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")));
 
+        //Обрабатываем ошибку
         try (GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(String.format("/log/%s", filename)));
              BufferedReader br = new BufferedReader(new InputStreamReader(gzis))) {
 
             StringBuilder content = new StringBuilder();
             String line;
 
-
+            // читаем содержимое архива
             while ((line = br.readLine()) != null) {
                 content.append(line);
             }
 
-
+            // сохраняем содержимое во временный файл
             File tempFile = new File("temp.txt");
             try (FileOutputStream fos = new FileOutputStream(tempFile)) {
                 fos.write(content.toString()
                                  .getBytes());
             }
 
-
+            // подсчитываем количество строк, содержащих слово "Success"
             int count = 0;
             try (BufferedReader tempFileReader = new BufferedReader(new InputStreamReader(new FileInputStream(tempFile)))) {
                 while ((line = tempFileReader.readLine()) != null) {
@@ -41,10 +45,10 @@ public class Main {
                 }
             }
 
-
+            // выводим результат
             System.out.printf("Количество строк со словом 'Success': %d%n", count);
 
-
+            // удаляем временный файл
             if (!tempFile.delete()) {
                 System.err.println("Ошибка при удалении временного файла");
             }
@@ -54,4 +58,3 @@ public class Main {
         }
     }
 }
-
